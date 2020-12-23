e3598e6e2acb797a8bc831e217ad1f308240083e
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2020-12-23 13:33:41 +0000

    Recompiled contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,9 +4,13 @@ const Fallback = artifacts.require("Fallback")
 const Fallout = artifacts.require("Fallout")
 const CoinFlip = artifacts.require("CoinFlip")
 const CoinFlipAttack = artifacts.require("CoinFlipAttack")
+const Telephone = artifacts.require("Telephone")
+const TelephoneAttack = artifacts.require("TelephoneAttack")
+const Token = artifacts.require("Token")
 
 const settings = {
-  contribution: 100000000000000
+  contribution: 100000000000000,
+  supply:21000000
 }
 
 module.exports = function(deployer) {
@@ -15,4 +19,7 @@ module.exports = function(deployer) {
   deployer.deploy(Fallout)
   deployer.deploy(CoinFlip)
   deployer.deploy(CoinFlipAttack)
+  deployer.deploy(Telephone)
+  deployer.deploy(TelephoneAttack)
+  deployer.deploy(Token, settings.supply)
 };
